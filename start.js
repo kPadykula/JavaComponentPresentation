997526f32f01e636dbd06d0aa918905cc606b084
kPadykula <k.padykula97@gmail.com>
2022-04-23 02:13:31 +0200

    updates to deploy to heroku

--- start.js
+++ start.js
@@ -2,19 +2,14 @@
 const express = require('express');
 const path = require('path');
 
-const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/java-component-presentation'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/java-component-presentation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
